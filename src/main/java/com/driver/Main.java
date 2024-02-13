038e19a2c88eca459dba304e1cee9b60595ebd30
Parth <parthudgirkar@gmail.com>
2024-02-13 15:20:22 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,17 +5,17 @@ public class Main {
         B obj = new B();
         System.out.println(obj.meth());
     }
-}
 
-class A {
-    public String meth(){
-        return "Invoking method from class A";
+    static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
     }
-}
 
-class B extends A{
-    @Override
-    public String meth(){
-        return "Method is overridden in Extendend class B";
+    static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
     }
 }
